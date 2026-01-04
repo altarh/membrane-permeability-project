98291d93eb99117b8ba42021934ee2c736fc57f7
altarh <altarh@mail.tau.ac.il>
2026-01-04 15:50:16 +0200

    changed Class_Label to be PAMPA

--- data_loading.py
+++ data_loading.py
@@ -7,5 +7,5 @@ def read_file_and_add_Class_Label(csv_path='CycPeptMPDB_First30.csv'):
     df = pd.read_csv(csv_path, sep=",")
     threshold = -6.0
     # If Permeability is greater than -6, it's 1 (Active), otherwise 0
-    df['Class_Label'] = df['Permeability'].apply(lambda x: 1 if x > threshold else 0)
+    df['Class_Label'] = df['PAMPA']#.apply(lambda x: 1 if x > threshold else 0)
     return df
